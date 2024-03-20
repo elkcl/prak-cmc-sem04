18b7dcd2038f3ddb8c7a60ba8352396b85b23417
elkcl <28754537+elkcl@users.noreply.github.com>
2024-03-20 22:19:10 +0300

    mz04-1 and mz04-2 reject fix

--- mz04/mz04-1.cpp
+++ mz04/mz04-1.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cstdint>
+#include <sys/types.h>
 #include <vector>
 
 using std::vector;
@@ -8,7 +10,7 @@ process(const vector<uint64_t> &v1, vector<uint64_t> &v2, size_t step)
 {
     auto it1 = v1.cbegin();
     auto it2 = v2.rbegin();
-    for (; it1 < v1.cend() && it2 < v2.rend(); it1 += step, ++it2) {
+    for (; it1 < v1.cend() && it2 < v2.rend(); it1 += std::min(static_cast<ssize_t>(step), v1.cend() - it1), ++it2) {
         *it2 += *it1;
     }
 }
--- mz04/mz04-2.cpp
+++ mz04/mz04-2.cpp
@@ -6,7 +6,13 @@ using std::vector;
 void
 process(vector<int64_t> &v, int64_t threshold)
 {
-    v.reserve(v.size() * 2);
+    size_t ext = 0;
+    for (auto it = v.rbegin(); it != v.rend(); ++it) {
+        if (*it >= threshold) {
+            ++ext;
+        }
+    }
+    v.reserve(v.size() + ext);
     for (auto it = v.rbegin(); it != v.rend(); ++it) {
         if (*it >= threshold) {
             v.push_back(*it);
